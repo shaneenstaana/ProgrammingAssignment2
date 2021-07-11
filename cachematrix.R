401798975932359c2a3eb41e4086555f55fd0cb9
shaneenstaana <87257635+shaneenstaana@users.noreply.github.com>
2021-07-11 15:28:31 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## A requirement to fulfill Coursera R Programming course
+## shaneenstaana
+## The 'x' is input as matrix
+## The "tr" is set as null
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  tr <- NULL
+  set <- function(y){
+    x <<- y
+    tr <<- NULL
+  }
+  get <- function() {x}
+  settranslate <- function(translate) {tr <<- translate}
+  gettranslate <- function() {tr}
+  list(set = set, get = get, settranslate = settranslate, gettranslate = gettranslate)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##translates the matrix
+cachetranslate <- function(x, ...){
+  tr <- x$gettranslate()
+  if(!is.null(tr)) {
+    message ("acquiring cache data")
+    return(tr)
+  }
+  inf <-x$get()
+  tr <- translate(inf, ...)
+  x$settranslate(tr)
+  tr
 }
